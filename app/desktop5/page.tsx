a3bf2f5939dc72426a0c0d1d5e63fcab8c8d1eb1
GreenEcoGenius <GreenEcoGenius>
2026-03-23 00:17:03 +0000

    Pushed from Locofy Builder

---
+++ app/desktop5/page.tsx
@@ -0,0 +1,5 @@
+import Desktop5 from "./desktop5";
+
+export default function Page() {
+  return <Desktop5 />;
+}
---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import "./global.css";
+
+import { ReactNode } from "react";
+
+export const metadata = {
+  title: `Skin—Clinic _ Figma Design E-commerce Template`,
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>{children}</body>
+    </html>
+  );
+}
