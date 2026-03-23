c84c18e0261b56eca5d24e2f1180f62ba566ed20
GreenEcoGenius <GreenEcoGenius>
2026-03-23 01:14:24 +0000

    Pushed from Locofy Builder

--- app/desktop4/page.tsx
+++
@@ -1,5 +0,0 @@
-import Desktop4 from "./desktop4";
-
-export default function Page() {
-  return <Desktop4 />;
-}
--- app/desktop6/page.tsx
+++
@@ -1,5 +0,0 @@
-import Desktop6 from "./desktop6";
-
-export default function Page() {
-  return <Desktop6 />;
-}
